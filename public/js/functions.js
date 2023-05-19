e8de94f0ef67946424a11939c9dd2ff7e4f70dce
dele-rodriguez <114116037+dele-rodriguez@users.noreply.github.com>
2023-05-19 11:26:45 +0100

    i styled the user's homepage and made it responsive. I also changed some css stylr to make them more global and reusable

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     activeLinkControl();
     toggler();
     passwordtoText();
+    copyToClipboard();
+    shareProfile();
+    disableLink();
 });
 
 function activeLinkControl() {
@@ -35,4 +38,38 @@ function passwordtoText() {
         togglePassword.removeClass('show-password');
       }
     });
+}
+function copyToClipboard() {
+    $('.fa-copy').on('click', () => {
+        var linkText = $('.special-link').attr('href');
+        
+        navigator.clipboard.writeText(linkText)
+          .then(() => {
+            alert("Copied link to clipboard!");
+            // You can add additional feedback or notification here
+          })
+          .catch(function(error) {
+            console.error('Failed to copy link:', error);
+        });
+    });   
+}
+function shareProfile() {
+    $('.share-btn').click(() => {
+        // Get the unique URL and brief information
+        const uniqueLink = $('.share-profile').attr('href');
+        const url = encodeURIComponent(uniqueLink);
+        const info = "Write a secret anonymous message for me.. 😉 I won't know who wrote it.. 😂❤👉  ";
+  
+        // Construct the WhatsApp message
+        const message = `${info} ${url}`;
+  
+        // Open WhatsApp with the pre-populated message
+        window.open(`https://wa.me/?text=${message}`);
+    });
+}
+function disableLink() {
+    $('.share-profile').click((event) => {
+        event.preventDefault(); // Prevent the default behavior of the link
+        // Your custom logic here
+      });
 }
